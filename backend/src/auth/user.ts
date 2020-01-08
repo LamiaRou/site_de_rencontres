946d678d183c6af2158af179443446d40e838ade
LROUMILI <roumililamia23@gmail.com>
2020-01-08 14:25:57 +0100

    update db

--- backend/src/auth/user.ts
+++ backend/src/auth/user.ts
@@ -6,9 +6,25 @@ export class User {
   id: number;
 
   @Column()
+  fname: string;
+
+  @Column()
+  lname: string;
+
+
+  @Column()
+  address: string;
+
+  @Column()
   email: string;
 
   @Column()
   password: string;
+
+  @Column()
+  gender: string;
+
+  @Column()
+  date: string;
 
 }
